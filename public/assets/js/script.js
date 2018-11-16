da43d510eb5883f748d8071b5fd5ad4586c014c9
Jacson Santos <jacsonk47@gmail.com>
2018-11-16 17:06:41 -0300

    correcao validacao de data v3

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -27,7 +27,7 @@ function loadCliente(token) {
 }
 // Mask INPUT's
 function mask_input() {
-    $('.date').mask('00/00/0000');
+    $('.dated').mask('00/00/0000');
     $('.cep').mask('00000-000');
     $('.tel-cel').mask('(00) 00000-0000');
     $('.tel-fixo').mask('(00) 0000-0000');
@@ -58,6 +58,12 @@ function validator(form) {
             telefone_celular: "Informe seu Telefone Celular"
         }
     })
+
+    jQuery.validator.addMethod('checkDateFormat', function(value, element){
+        var stringPattern = /^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))1|(?:(?:29|30)(\/|-|\.)(?:0?[1,3-9]|1[0-2])2))(?:(?:1[6-9]|[2-9]d)?d{2})$|^(?:29(\/|-|\.)0?23(?:(?:(?:1[6-9]|[2-9]d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))4(?:(?:1[6-9]|[2-9]d)?d{2})$/gm;
+        if(stringPattern.test(value)){ return true; }
+        else { return false; }
+    },"Informe sua Data de Nascimento");
 }
 // Bootstrap
 $(function (e) {
